9f9fdb326975d953909d361811d950e6b6a3504e
Abu Baker Kahtan <abumkahtan@gmail.com>
2021-11-25 15:22:11 +0000

    Initialising the Cocktail Database Project

---
+++ src/main/java/com/tsi/kahtan/abubakr/cocktaildemo/cocktailDbDemo/CocktailDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tsi.kahtan.abubakr.cocktaildemo.cocktailDbDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsi/kahtan/abubakr/cocktaildemo/cocktailDbDemo/CocktailDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsi.kahtan.abubakr.cocktaildemo.cocktailDbDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
