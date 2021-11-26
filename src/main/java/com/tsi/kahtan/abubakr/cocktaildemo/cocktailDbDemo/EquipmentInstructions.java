49d9af46ec3652ec60a3b5091afe1d4952b75f52
Abu Baker Kahtan <abumkahtan@gmail.com>
2021-11-26 09:29:33 +0000

    added all the middle classes and their attributes

---
+++ src/main/java/com/tsi/kahtan/abubakr/cocktaildemo/cocktailDbDemo/EquipmentInstructions.java
@@ -0,0 +1,28 @@
+package com.tsi.kahtan.abubakr.cocktaildemo.cocktailDbDemo;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EquipmentInstructions {
+    private List<EquipmentInstructions> equipmentInstructionsList = new ArrayList<>();
+    private int instructionID;
+    private int equipmentID;
+
+    public EquipmentInstructions(int instructionID, int equipmentID){
+        this.instructionID = instructionID;
+        this.equipmentID = equipmentID;
+    }
+
+    public void populatingIngredientList(){
+        this.equipmentInstructionsList.add(new EquipmentInstructions(0, 0));
+    }
+    public List<EquipmentInstructions> getEquipmentInstructionsList(){
+        return this.equipmentInstructionsList;
+    }
+    public String toString(){
+        String json = new Gson().toJson(equipmentInstructionsList);
+        return json;
+    }
+}
---
+++ src/main/java/com/tsi/kahtan/abubakr/cocktaildemo/cocktailDbDemo/IngredientInstructions.java
@@ -0,0 +1,31 @@
+package com.tsi.kahtan.abubakr.cocktaildemo.cocktailDbDemo;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IngredientInstructions {
+
+
+    private List<IngredientInstructions> ingredientInstructionsList = new ArrayList<>();
+    private int instructionID;
+    private int ingredientID;
+
+    public IngredientInstructions(int instructionID, int ingredientID){
+        this.instructionID = instructionID;
+        this.ingredientID = ingredientID;
+    }
+
+    public void populatingIngredientList(){
+        this.ingredientInstructionsList.add(new IngredientInstructions(0, 0));
+    }
+    public List<IngredientInstructions> getIngredientInstructionsList(){
+        return this.ingredientInstructionsList;
+    }
+    public String toString(){
+        String json = new Gson().toJson(ingredientInstructionsList);
+        return json;
+    }
+    
+}
