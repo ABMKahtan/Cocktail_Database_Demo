1389088c3e729bb74d95f7b8906044c11f91a794
Abu Baker Kahtan <abumkahtan@gmail.com>
2022-01-04 13:13:30 +0000

    changed the settings to connect to the aws database

--- src/main/java/com/tsi/kahtan/abubakr/cocktaildemo/cocktailDbDemo/CocktailDbDemoApplication.java
+++ src/main/java/com/tsi/kahtan/abubakr/cocktaildemo/cocktailDbDemo/CocktailDbDemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/CocktailsDB")
 public class CocktailDbDemoApplication {
-    private static SecretGetter secretDB = new SecretGetter();
 
     @Autowired
     private CocktailRepository cocktailRepository;
@@ -23,7 +22,6 @@ public class CocktailDbDemoApplication {
     private IngredientRepository ingredientRepository;
 
     public static void main(String[] args) {
-        secretDB.getSecret();
         SpringApplication.run(CocktailDbDemoApplication.class, args);
 
     }
